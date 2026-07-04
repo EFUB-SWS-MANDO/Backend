c4aa63fe757da6e34992a7a3fdf619d032fee98d
corjqnrl <coqnrl@ewha.ac.kr>
2026-07-04 20:42:15 +0900

    feat(SPROUT-72): resume 엔티티 구현

--- src/main/java/com/example/sprout/domain/resume/entity/Resume.java
+++ src/main/java/com/example/sprout/domain/resume/entity/Resume.java
@@ -1,6 +1,33 @@
 package com.example.sprout.domain.resume.entity;
 
+import com.example.sprout.domain.member.entity.Member;
 import com.example.sprout.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Resume extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private Member author;
+
+    @Builder
+    public Resume(String title, Member author) {
+        this.title = title;
+        this.author = author;
+    }
 }
