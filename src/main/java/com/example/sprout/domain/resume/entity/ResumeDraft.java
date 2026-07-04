c6c8d04c428c1f9add8165fcf5523d8c277ff605
corjqnrl <coqnrl@ewha.ac.kr>
2026-07-04 20:56:56 +0900

    feat(SPROUT-73): ResumeDraft 엔티티 구현

--- src/main/java/com/example/sprout/domain/resume/entity/ResumeDraft.java
+++ src/main/java/com/example/sprout/domain/resume/entity/ResumeDraft.java
@@ -1,6 +1,44 @@
 package com.example.sprout.domain.resume.entity;
 
 import com.example.sprout.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ResumeDraft extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "resume_id", nullable = false)
+    private Resume resume;
+
+    @Column(name = "order_index", nullable = false)
+    private Long orderIndex;
+
+    @Column(nullable = false)
+    private String question;
+
+    @Column
+    private String answer;
+
+    @Column
+    private String description;
+
+    @Builder
+    public ResumeDraft(Resume resume, Long orderIndex, String question, String answer, String description) {
+        this.resume = resume;
+        this.orderIndex = orderIndex;
+        this.question = question;
+        this.answer = answer;
+        this.description = description;
+    }
 }
