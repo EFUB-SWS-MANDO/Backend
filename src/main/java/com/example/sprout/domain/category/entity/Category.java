5f2f8af2bf1f5c5e71664447baa9c067251a71ab
전채연 <ami02074657@gmail.com>
2026-07-05 01:48:14 +0900

    feat(SPROUT-75): Category 엔티티 구성 및 Enum 생성

--- src/main/java/com/example/sprout/domain/category/entity/Category.java
+++ src/main/java/com/example/sprout/domain/category/entity/Category.java
@@ -1,6 +1,30 @@
 package com.example.sprout.domain.category.entity;
 
+import com.example.sprout.domain.category.enums.CategoryType;
 import com.example.sprout.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Getter
+@Table(name = "categories")
+@NoArgsConstructor
 public class Category extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private CategoryType type;
+
+    @Builder
+    public Category(CategoryType type) {
+        this.type = type;
+    }
+
 }
