cc62a753d73ba978faa97354c34d66aa689bf27b
sum-young <summeryoung@ewhain.net>
2026-07-05 15:47:42 +0900

    feat(SPROUT-80): Member 엔티티 및 OauthProvider enum 생성

--- src/main/java/com/example/sprout/domain/member/entity/Member.java
+++ src/main/java/com/example/sprout/domain/member/entity/Member.java
@@ -1,6 +1,41 @@
 package com.example.sprout.domain.member.entity;
 
+import com.example.sprout.domain.member.enums.OauthProvider;
 import com.example.sprout.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Getter
+@Table(name = "members",
+        indexes = {
+            @Index(name = "idx_member_oauth", columnList="oauth_provider, oauth_id")
+        },
+        uniqueConstraints = {
+            @UniqueConstraint(columnNames = {"oauth_provider", "oauth_id"})
+        }
+)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "oauth_provider", nullable = false)
+    private OauthProvider oauthProvider;
+
+    @Column(name = "oauth_id", nullable = false)
+    private String oauthId;
+
+    @Builder
+    public Member (String oauthId, OauthProvider oauthProvider) {
+        this.oauthId = oauthId;
+        this.oauthProvider = oauthProvider;
+    }
 }
---
+++ src/main/java/com/example/sprout/domain/member/enums/OauthProvider.java
@@ -0,0 +1,6 @@
+package com.example.sprout.domain.member.enums;
+
+//TODO: 사용할 소셜 로그인 (추후 확정된 후 수정 가능)
+public enum OauthProvider {
+    KAKAO
+}
