b13445d62d038875c9f2e448e36d89c745bfc327
sum-young <summeryoung@ewhain.net>
2026-07-05 17:09:00 +0900

    feat(SPROUT-82): Post 및 PostCategory 엔티티 구현

--- src/main/java/com/example/sprout/domain/post/entity/Post.java
+++ src/main/java/com/example/sprout/domain/post/entity/Post.java
@@ -1,6 +1,54 @@
 package com.example.sprout.domain.post.entity;
 
+import com.example.sprout.domain.member.entity.Member;
 import com.example.sprout.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Getter
+@Table(name = "posts",
+        indexes = {
+            @Index(name="idx_post_author_id", columnList = "author_id")
+        }
+)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false, updatable = false)
+    private Member author;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(name = "like_count")
+    private int likeCount;
+
+    @Builder
+    public Post(Member author, String title, String content) {
+        this.author = author;
+        this.title = title;
+        this.content = content;
+        this.likeCount = 0;
+    }
+
+    public void updatePost(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void addLikeCount() {
+        this.likeCount += 1;
+    }
 }
