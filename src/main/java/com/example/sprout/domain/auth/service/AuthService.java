977a4d2d0d13a0a77b6b9d5ae4691000a635c84c
sum-young <summeryoung@ewhain.net>
2026-07-07 11:33:53 +0900

    feat(SPROUT-17): 카카오 로그인 서비스 및 API 구현

---
+++ src/main/java/com/example/sprout/domain/auth/service/AuthService.java
@@ -0,0 +1,65 @@
+package com.example.sprout.domain.auth.service;
+
+import com.example.sprout.domain.auth.client.OauthApiClient;
+import com.example.sprout.domain.auth.client.OauthUserInfo;
+import com.example.sprout.domain.auth.dto.request.CreateMemberRequest;
+import com.example.sprout.domain.auth.dto.response.CreateMemberResponse;
+import com.example.sprout.domain.auth.jwt.JwtUtil;
+import com.example.sprout.domain.member.entity.Member;
+import com.example.sprout.domain.member.enums.OauthProvider;
+import com.example.sprout.domain.member.repository.MemberRepository;
+import com.example.sprout.global.error.BusinessException;
+import com.example.sprout.global.error.GlobalErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class AuthService {
+    private final List<OauthApiClient> oauthApiClientList;
+    private final MemberRepository memberRepository;
+    private final JwtUtil jwtUtil;
+
+    public CreateMemberResponse signIn(CreateMemberRequest request) {
+        OauthApiClient client = findClient(request.provider());
+        OauthUserInfo userInfo = client.getUserInfo(request.kakaoAccessToken());
+
+        Optional<Member> existingMember = memberRepository.findByOauthIdAndOauthProvider(userInfo.getProviderId(), request.provider());
+        boolean isNewUser = existingMember.isEmpty();
+
+        Member member = existingMember.orElseGet(() -> memberRepository.save(
+                        Member.builder()
+                                .oauthProvider(request.provider())
+                                .oauthId(userInfo.getProviderId())
+                                .build()
+                ));
+
+        String accessToken = jwtUtil.createAccessToken(member.getId());
+        String refreshToken = jwtUtil.createRefreshToken(member.getId());
+
+        long expiresIn = jwtUtil.getAccessExpirationMs()/1000;
+        LocalDateTime expiresAt = LocalDateTime.now().plusSeconds(expiresIn);
+
+        return CreateMemberResponse.builder()
+                .memberId(member.getId())
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .expiresIn(expiresIn)
+                .expiresAt(expiresAt)
+                .isNewUser(isNewUser)
+                .build();
+    }
+
+    private OauthApiClient findClient(OauthProvider provider) {
+        return oauthApiClientList.stream()
+                .filter(client -> client.supportedProvider() == provider)
+                .findFirst()
+                .orElseThrow(() -> new BusinessException(GlobalErrorCode.UNSUPPORTED_PROVIDER));
+    }
+}
