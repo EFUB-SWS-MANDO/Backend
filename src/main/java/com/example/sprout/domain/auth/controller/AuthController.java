78786368782f5332718625ee970f1d4d312688e5
sum-young <summeryoung@ewhain.net>
2026-07-07 20:17:35 +0900

    chore(SPROUT-17): AuthController 로그인 요청 로깅 추가

--- src/main/java/com/example/sprout/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/sprout/domain/auth/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.example.sprout.domain.auth.dto.response.CreateMemberResponse;
 import com.example.sprout.domain.auth.service.AuthService;
 import com.example.sprout.global.common.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,12 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@Slf4j
 public class AuthController {
 
     private final AuthService authService;
 
     @PostMapping("/sign-in")
     public ResponseEntity<ApiResponse<CreateMemberResponse>> signIn(@RequestBody CreateMemberRequest request) {
+
+        log.info("로그인 요청 - provider: {}", request.provider());
         CreateMemberResponse response = authService.signIn(request);
 
         return ResponseEntity.ok(ApiResponse.success("로그인 성공", response));
