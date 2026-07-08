1c6e873ac95666ac64972599c561d5a65c294dbb
sum-young <summeryoung@ewhain.net>
2026-07-08 12:48:51 +0900

    feat(SPROUT-10): JwtUtil 토큰 검증 로직 추가

--- src/main/java/com/example/sprout/domain/auth/jwt/JwtUtil.java
+++ src/main/java/com/example/sprout/domain/auth/jwt/JwtUtil.java
@@ -1,7 +1,9 @@
 package com.example.sprout.domain.auth.jwt;
 
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -45,4 +47,43 @@ public class JwtUtil {
     }
 
     public Long getAccessExpirationMs() {return accessExpirationMs;}
+
+    //토큰 유효성 검증
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token);
+
+            return true;
+        } catch (ExpiredJwtException e) {
+            //만료된 토큰
+            return false;
+        } catch (MalformedJwtException | SignatureException | UnsupportedJwtException e) {
+            //형식이 잘못된 경우/서명이 다른 경우/미지원 토큰
+            return false;
+        } catch (IllegalArgumentException e) {
+            //토큰이 비거나 null인 경우
+            return false;
+        }
+    }
+
+    //토큰에서 memberId 추출
+    public Long getMemberId(String token) {
+        Claims claims = Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+        return claims.get("memberId", Long.class);
+    }
+
+    //HTTP 요청 헤더에서 토큰 추출
+    public static String resolveToken(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        if (header != null && header.startsWith("Bearer ")) return header.substring(7);
+        return null;
+    }
 }
