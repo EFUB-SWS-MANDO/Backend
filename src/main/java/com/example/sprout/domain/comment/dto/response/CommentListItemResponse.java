8f2d73ea981c8fbb06466a37bd50abda2381f24b
corjqnrl <coqnrl@ewha.ac.kr>
2026-07-10 18:12:37 +0900

    feat(SPROUT-35): 대댓글 존재 여부 반환을 위해 댓글 조회 목록 response 분리

---
+++ src/main/java/com/example/sprout/domain/comment/dto/response/CommentListItemResponse.java
@@ -0,0 +1,16 @@
+package com.example.sprout.domain.comment.dto.response;
+
+import com.example.sprout.domain.comment.entity.Comment;
+import com.example.sprout.domain.profile.entity.Profile;
+
+public record CommentListItemResponse(
+        CommentResponse commentResponse,
+        boolean hasChildren
+) {
+    public static CommentListItemResponse of(Comment comment, Profile authorProfile, boolean hasChildren) {
+        return new CommentListItemResponse(
+                CommentResponse.of(comment, authorProfile),
+                hasChildren
+        );
+    }
+}
