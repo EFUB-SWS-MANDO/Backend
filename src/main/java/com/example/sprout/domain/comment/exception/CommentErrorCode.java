a4b613ed3b2e279b2e540be66e913d6c08e38453
corjqnrl <coqnrl@ewha.ac.kr>
2026-07-10 18:10:17 +0900

    feat(SPROUT-35): 부모 댓글이 삭제됐을 경우 대댓글 생성 불가 에러 코드

--- src/main/java/com/example/sprout/domain/comment/exception/CommentErrorCode.java
+++ src/main/java/com/example/sprout/domain/comment/exception/CommentErrorCode.java
@@ -11,7 +11,8 @@ public enum CommentErrorCode implements ErrorCode {
 
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 댓글입니다."),
     COMMENT_ACCESS_DENINED(HttpStatus.FORBIDDEN, "권한이 없습니다."),
-    PARENT_NOT_IN_POST(HttpStatus.BAD_REQUEST, "부모 댓글이 해당 게시글에 존재하지 않습니다.");
+    PARENT_NOT_IN_POST(HttpStatus.BAD_REQUEST, "부모 댓글이 해당 게시글에 존재하지 않습니다."),
+    CANNOT_REPLY_TO_DELETED_COMMENT(HttpStatus.BAD_REQUEST, "삭제된 댓글에는 대댓글을 달 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
