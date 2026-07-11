f1c528d298826c388eb81327cf1189351c5b7b00
corjqnrl <coqnrl@ewha.ac.kr>
2026-07-11 19:18:48 +0900

    refactor(SPROUT-37): @RequestParam -> @ModelAttribute로 수정

--- src/main/java/com/example/sprout/domain/comment/controller/CommentController.java
+++ src/main/java/com/example/sprout/domain/comment/controller/CommentController.java
@@ -1,5 +1,6 @@
 package com.example.sprout.domain.comment.controller;
 
+import com.example.sprout.domain.auth.security.AuthMember;
 import com.example.sprout.domain.comment.dto.request.CreateCommentRequest;
 import com.example.sprout.domain.comment.dto.response.CommentResponse;
 import com.example.sprout.domain.comment.service.CommentService;
@@ -21,12 +22,10 @@ public class CommentController {
 
     // 댓글 생성
     @PostMapping("/posts/{postId}/comments")
-    public ResponseEntity<ApiResponse<CommentResponse>> createComment(@PathVariable("postId") Long postId,
+    public ResponseEntity<ApiResponse<CommentResponse>> createComment(@AuthMember Long requesterId,
+                                                                      @PathVariable("postId") Long postId,
                                                                       @Valid @RequestBody CreateCommentRequest request) {
         log.info("댓글 생성 API 호출 - postId: {}", postId);
-
-        // TODO: 나중에 memberId 추출해서 사용하는 걸로 변경
-        Long requesterId = 1L;
 
         CommentResponse response = commentService.createComment(requesterId, postId, request);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success("댓글 생성 성공", response));
@@ -34,12 +33,10 @@ public class CommentController {
 
     // 댓글 삭제
     @DeleteMapping("/comments/{commentId}")
-    public ResponseEntity<ApiResponse<Void>> deleteComment(@PathVariable("commentId") Long commentId) {
+    public ResponseEntity<ApiResponse<Void>> deleteComment(@AuthMember Long requesterId,
+                                                           @PathVariable("commentId") Long commentId) {
 
         log.info("댓글 삭제 API 요청 - commentId: {}", commentId);
-
-        // TODO 사용자 ID 추후 @AuthMember 사용하여 변경
-        Long requesterId = 1L;
 
         commentService.deleteComment(requesterId, commentId);
 
