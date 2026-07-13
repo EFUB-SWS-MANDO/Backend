d2f3bcab5b497e5e1de24efa059efd2c3b67d715
sum-young <summeryoung@ewhain.net>
2026-07-13 21:11:44 +0900

    feat(SPROUT-21): ResumeDraft 삭제 로직 추가

--- src/main/java/com/example/sprout/domain/resume/repository/ResumeRepository.java
+++ src/main/java/com/example/sprout/domain/resume/repository/ResumeRepository.java
@@ -4,7 +4,10 @@ import com.example.sprout.domain.member.entity.Member;
 import com.example.sprout.domain.resume.entity.Resume;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ResumeRepository extends JpaRepository<Resume, Long> {
 
     void deleteAllByAuthor (Member author);
+    List<Resume> findAllByAuthor(Member member);
 }
--- src/main/java/com/example/sprout/domain/resume/service/ResumeService.java
+++ src/main/java/com/example/sprout/domain/resume/service/ResumeService.java
@@ -1,11 +1,14 @@
 package com.example.sprout.domain.resume.service;
 
 import com.example.sprout.domain.member.entity.Member;
+import com.example.sprout.domain.resume.entity.Resume;
 import com.example.sprout.domain.resume.repository.ResumeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,8 +17,16 @@ public class ResumeService {
 
     private final ResumeRepository resumeRepository;
 
+    private final ResumeDraftService resumeDraftService;
+
+    //회원 탈퇴 시 resume 및 resumeDraft 삭제
     @Transactional
     public void deleteByMember(Member member) {
+        //resumeDraft 삭제
+        List<Resume> resumeList = resumeRepository.findAllByAuthor(member);
+        resumeList.forEach(resumeDraftService::deleteAllByResume);
+
+        //resume 삭제
         resumeRepository.deleteAllByAuthor(member);
     }
 }
