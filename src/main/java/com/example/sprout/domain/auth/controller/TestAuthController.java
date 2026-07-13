aedd6c13ba725fc519f65cd8f2fd5ff0c36571d4
sum-young <summeryoung@ewhain.net>
2026-07-13 11:53:48 +0900

    feat(SPROUT-86): 테스트용 토큰 발급, 멤버 생성, 멤버의 토큰 발급 API 추가

--- src/main/java/com/example/sprout/domain/auth/controller/TestAuthController.java
+++ src/main/java/com/example/sprout/domain/auth/controller/TestAuthController.java
@@ -1,9 +1,13 @@
 package com.example.sprout.domain.auth.controller;
 
+import com.example.sprout.domain.auth.dto.response.TestTokenResponse;
 import com.example.sprout.domain.auth.jwt.JwtUtil;
 import com.example.sprout.domain.auth.security.AuthMember;
+import com.example.sprout.domain.auth.service.TestAuthService;
 import com.example.sprout.global.common.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.weaver.ast.Test;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,16 +16,33 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/test")
 @RequiredArgsConstructor
+@Slf4j
 public class TestAuthController {
 
-    private final JwtUtil jwtUtil;
+    private final TestAuthService testAuthService;
 
     //테스트 액세스 토큰 발급 API
     @PostMapping("/token")
-    public ResponseEntity<ApiResponse<String>> issueTestToken(@RequestParam("memberId") Long memberId) {
-        String testAuthToken = jwtUtil.createAccessToken(memberId);
+    public ResponseEntity<ApiResponse<TestTokenResponse>> issueTestToken() {
+        TestTokenResponse response = testAuthService.getTestTokenAndMember();
+
+        return ResponseEntity.ok(ApiResponse.success("테스트 멤버 생성 및 토큰 발급 성공", response));
+    }
+
+    @PostMapping("/member/{memberId}/token")
+    public ResponseEntity<ApiResponse<String>> getTestTokenByMember(@PathVariable Long memberId) {
+        log.info("테스트 토큰 발급 요청 - memberId: {}", memberId);
+        String testAuthToken = testAuthService.getTestTokenByMemberId(memberId);
 
         return ResponseEntity.ok(ApiResponse.success("테스트 토큰 발급", testAuthToken));
+    }
+
+    @PostMapping("/member")
+    public ResponseEntity<ApiResponse<Long>> createTestMember() {
+        log.info("테스트 멤버 생성 요청");
+        Long memberId = testAuthService.createTestMember();
+
+        return ResponseEntity.ok(ApiResponse.success("테스트 멤버 생성 성공", memberId));
     }
 
     @GetMapping
