a96f906aa596868d2fbb83eccaad5c40a8dbba32
corjqnrl <coqnrl@ewha.ac.kr>
2026-07-18 01:09:10 +0900

    feat(SPROUT-39): 자소서 목록 조회 controller 구현

--- src/main/java/com/example/sprout/domain/resume/controller/ResumeController.java
+++ src/main/java/com/example/sprout/domain/resume/controller/ResumeController.java
@@ -2,17 +2,17 @@ package com.example.sprout.domain.resume.controller;
 
 import com.example.sprout.domain.auth.security.AuthMember;
 import com.example.sprout.domain.resume.dto.request.CreateResumeRequest;
+import com.example.sprout.domain.resume.dto.request.GetResumeListCondition;
+import com.example.sprout.domain.resume.dto.response.GetResumeListResponse;
 import com.example.sprout.domain.resume.dto.response.ResumeResponse;
 import com.example.sprout.domain.resume.service.ResumeService;
 import com.example.sprout.global.common.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -30,5 +30,15 @@ public class ResumeController {
 
         ResumeResponse response = resumeService.createResume(requesterId, request);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success("자소서 생성 성공", response));
+    }
+
+    // 자소서 목록 조회
+    @GetMapping
+    public ResponseEntity<ApiResponse<GetResumeListResponse>> getResumeList(@AuthMember Long requesterId,
+                                                                            @ModelAttribute GetResumeListCondition condition) {
+        log.info("자소서 목록 조회 API 호출 - requesterId: {}", requesterId);
+
+        GetResumeListResponse response = resumeService.getResumeList(requesterId, condition.idAfter(), condition.limit(), condition.keyword());
+        return ResponseEntity.ok().body(ApiResponse.success("자소서 목록 조회 성공", response));
     }
 }
