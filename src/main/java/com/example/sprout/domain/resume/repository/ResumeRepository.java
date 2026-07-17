8894478577d29ba6cb4d5cbf28665dc7938ccc19
corjqnrl <coqnrl@ewha.ac.kr>
2026-07-18 01:08:39 +0900

    feat(SPROUT-39): 자소서 목록 작성자 + 키워드로 조회 및 총 개수 count 쿼리 생성

--- src/main/java/com/example/sprout/domain/resume/repository/ResumeRepository.java
+++ src/main/java/com/example/sprout/domain/resume/repository/ResumeRepository.java
@@ -2,7 +2,11 @@ package com.example.sprout.domain.resume.repository;
 
 import com.example.sprout.domain.member.entity.Member;
 import com.example.sprout.domain.resume.entity.Resume;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 import java.util.List;
 
@@ -10,4 +14,24 @@ public interface ResumeRepository extends JpaRepository<Resume, Long> {
 
     void deleteAllByAuthor (Member author);
     List<Resume> findAllByAuthor(Member member);
+
+    @Query("""
+            SELECT r FROM Resume r
+            WHERE r.author = :author
+            AND (:keyword IS NULL OR r.title ILIKE CONCAT('%', :keyword, '%'))
+            AND (:idAfter IS NULL OR r.id < :idAfter)
+            ORDER BY r.createdAt DESC
+           """)
+    List<Resume> findPageByAuthorAndKeyword(@Param("author") Member author,
+                                            @Param("idAfter") Long idAfter,
+                                            @Param("keyword") String keyword,
+                                            Pageable pageable);
+
+    @Query("""
+            SELECT count(r) FROM Resume r
+            WHERE r.author = :author
+            AND (:keyword IS NULL OR r.title ILIKE CONCAT('%', :keyword, '%'))
+           """)
+    Long countAllByAuthorAndKeyword(@Param("author") Member author,
+                                    @Param("keyword") String keyword);
 }
