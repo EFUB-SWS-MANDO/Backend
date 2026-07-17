9bbcd6966ed3e1c943cb44b99d5213aa39c50b9e
corjqnrl <coqnrl@ewha.ac.kr>
2026-07-17 22:54:39 +0900

    feat(SPROUT-38): Post, PostCategory 조회

--- src/main/java/com/example/sprout/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/sprout/domain/post/repository/PostRepository.java
@@ -3,10 +3,18 @@ package com.example.sprout.domain.post.repository;
 import com.example.sprout.domain.member.entity.Member;
 import com.example.sprout.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.yaml.snakeyaml.events.Event;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findAllByAuthor (Member author);
+
+    @Query("SELECT DISTINCT p FROM Post p " +
+            "LEFT JOIN FETCH p.postCategories " +
+            "WHERE p.id IN :ids")
+    List<Post> findAllByIdWithCategories(@Param("ids") List<Long> ids);
 }
