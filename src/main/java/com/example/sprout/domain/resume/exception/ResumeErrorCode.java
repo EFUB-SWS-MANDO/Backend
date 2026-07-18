d3b542277f94b25a1428ad4aeca3c0a03d6fbe80
corjqnrl <coqnrl@ewha.ac.kr>
2026-07-19 00:46:26 +0900

    feat(SPROUT-41): 자소서 에러 코드 생성

---
+++ src/main/java/com/example/sprout/domain/resume/exception/ResumeErrorCode.java
@@ -0,0 +1,19 @@
+package com.example.sprout.domain.resume.exception;
+
+import com.example.sprout.global.error.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ResumeErrorCode implements ErrorCode{
+
+    AI_ANSWER_MISSING(HttpStatus.FAILED_DEPENDENCY, "AI 응답이 누락되었습니다."),
+    AI_RESPONSE_PARSE_FAILED(HttpStatus.BAD_GATEWAY, "AI 응답 JSON 파싱에 실패했습니다."),
+    RESUME_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 자기소개서입니다."),
+    RESUME_ACCESS_DENIED(HttpStatus.UNAUTHORIZED, "권한이 없습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+}
