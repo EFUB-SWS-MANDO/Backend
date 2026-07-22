a772b9ed1db3990eb82db242e4eea2ca5c9b77dc
corjqnrl <coqnrl@ewha.ac.kr>
2026-07-22 20:48:41 +0900

    refactor(SPROUT-91): S3 버킷명 수정

--- src/main/java/com/example/sprout/global/config/S3Config.java
+++ src/main/java/com/example/sprout/global/config/S3Config.java
@@ -9,6 +9,7 @@ import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class S3Config {
@@ -30,6 +31,26 @@ public class S3Config {
 
         // 배포(EC2): 키가 없으면 인스턴스 프로파일(Role) 자동 인식
         return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner (
+            @Value("${cloud.aws.credentials.access-key}") String accessKey,
+            @Value("${cloud.aws.credentials.secret-key}") String secretKey,
+            @Value("${cloud.aws.region}") String region
+    ) {
+        if (!accessKey.isBlank() && !secretKey.isBlank()) {
+            AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+            return S3Presigner.builder()
+                    .region(Region.of(region))
+                    .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                    .build();
+        }
+
+        return S3Presigner.builder()
                 .region(Region.of(region))
                 .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
