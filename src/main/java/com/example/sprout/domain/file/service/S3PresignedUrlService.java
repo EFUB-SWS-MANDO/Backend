f3d7da4600cf818868b540f3d32c464606fb50f2
corjqnrl <coqnrl@ewha.ac.kr>
2026-07-22 22:17:14 +0900

    feat(SPROUT-91): 게시글 상세조회용 presigned URL 발급 및 s3 파일 삭제 메서드 추가

--- src/main/java/com/example/sprout/domain/file/service/S3PresignedUrlService.java
+++ src/main/java/com/example/sprout/domain/file/service/S3PresignedUrlService.java
@@ -3,25 +3,36 @@ package com.example.sprout.domain.file.service;
 import com.example.sprout.domain.file.dto.request.PresignedUrlRequest;
 import com.example.sprout.domain.file.dto.response.PresignedUrlResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectAclRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class S3PresignedUrlService {
 
     private final S3Presigner s3Presigner;
+    private final S3Client s3Client;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
+    // presignedURL 발급
     public PresignedUrlResponse createPresignedUrl(Long requesterId, PresignedUrlRequest request) {
         String fileKey = "posts/temp/" + requesterId + "_" + UUID.randomUUID() + "_" + request.fileName();
 
@@ -39,5 +50,42 @@ public class S3PresignedUrlService {
         PresignedPutObjectRequest presigned = s3Presigner.presignPutObject(presignedRequest);
 
         return PresignedUrlResponse.of(presigned.url().toString(), fileKey);
+    }
+
+    // 게시글 상세 조회용 Presigned URL 발급
+    public String createDownloadUrl(String s3Key) {
+        GetObjectRequest objectRequest = GetObjectRequest.builder()
+                .bucket(bucket)
+                .key(s3Key)
+                .build();
+
+        GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(10))
+                .getObjectRequest(objectRequest)
+                .build();
+
+        PresignedGetObjectRequest presigned = s3Presigner.presignGetObject(presignRequest);
+        return presigned.url().toString();
+    }
+
+    // 여러 파일 한 번에 처리
+    public List<String> createDownloadUrls(List<String> s3Keys) {
+        return s3Keys.stream()
+                .map(this::createDownloadUrl)
+                .toList();
+    }
+
+    // 삭제
+    public void deleteFiles(List<String> s3Keys) {
+        for (String key : s3Keys) {
+            try {
+                s3Client.deleteObject(DeleteObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(key)
+                    .build());
+            } catch (Exception e) {
+            log.warn("S3 파일 삭제 실패: key={}", key, e);
+            }
+        }
     }
 }
