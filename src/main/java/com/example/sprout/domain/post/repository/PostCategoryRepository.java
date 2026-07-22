7454b4dbe5fad6cb6a1ce1f9af2d68fc675e6b03
sum-young <summeryoung@ewhain.net>
2026-07-23 03:03:19 +0900

    feat(SPROUT-28): 게시글 및 연관 엔티티 조회용 Repository 쿼리 추가

--- src/main/java/com/example/sprout/domain/post/repository/PostCategoryRepository.java
+++ src/main/java/com/example/sprout/domain/post/repository/PostCategoryRepository.java
@@ -2,11 +2,18 @@ package com.example.sprout.domain.post.repository;
 
 import com.example.sprout.domain.post.entity.Post;
 import com.example.sprout.domain.post.entity.PostCategory;
+import com.example.sprout.domain.post.summary.PostCategoryView;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PostCategoryRepository extends JpaRepository<PostCategory, Long> {
     List<PostCategory> findAllByPost(Post post);
     void deleteAllByPost(Post post);
+    @Query("SELECT pc.post.id AS postId, pc.category.type AS type " +
+            "FROM PostCategory  pc " +
+            "WHERE pc.post.id IN :postIds")
+    List<PostCategoryView> findCategoryTypesByPostIdIn(@Param("postIds") List<Long> postIds);
 }
