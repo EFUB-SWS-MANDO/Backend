bfe983024f5ed521a82b41f4a8451e7ac7b61072
corjqnrl <coqnrl@ewha.ac.kr>
2026-07-23 20:27:54 +0900

    feat(SPROUT-92): 댓글 생성/수정 request body isPrivate 필드 검증 추가

--- src/main/java/com/example/sprout/domain/comment/dto/request/CreateCommentRequest.java
+++ src/main/java/com/example/sprout/domain/comment/dto/request/CreateCommentRequest.java
@@ -4,6 +4,7 @@ import com.example.sprout.domain.comment.entity.Comment;
 import com.example.sprout.domain.member.entity.Member;
 import com.example.sprout.domain.post.entity.Post;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record CreateCommentRequest(
@@ -11,6 +12,8 @@ public record CreateCommentRequest(
         @Size(max = 300, message = "댓글은 300자를 초과할 수 없습니다.")
         String content,
         Long parentId,
+
+        @NotNull
         boolean isPrivate
 ) {
     public Comment toEntity(Member author, Post post, Comment parent, boolean isReplyPrivate) {
--- src/main/java/com/example/sprout/domain/comment/dto/request/UpdateCommentRequest.java
+++ src/main/java/com/example/sprout/domain/comment/dto/request/UpdateCommentRequest.java
@@ -1,11 +1,14 @@
 package com.example.sprout.domain.comment.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record UpdateCommentRequest(
         @NotBlank(message = "댓글 내용을 입력해주세요.")
         @Size(max = 300, message = "댓글은 300자를 초과할 수 없습니다.")
         String content,
+
+        @NotNull
         boolean isPrivate
 ) {}
