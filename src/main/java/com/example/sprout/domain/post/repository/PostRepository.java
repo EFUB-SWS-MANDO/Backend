bb6cdd48589786d7f64e3a146593eac86db437e7
sum-young <summeryoung@ewhain.net>
2026-07-24 23:28:31 +0900

    feat(SPROUT-90): 좋아요 생성 및 삭제 쿼리 수정

--- src/main/java/com/example/sprout/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/sprout/domain/post/repository/PostRepository.java
@@ -13,12 +13,22 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
 
     List<Post> findAllByAuthor (Member author);
-    @Modifying
-    @Query("UPDATE Post p SET p.likeCount = p.likeCount + 1 WHERE p.id = :postId")
+    @Modifying(clearAutomatically = true)
+    @Query("""
+            UPDATE Post p 
+            SET p.likeCount = p.likeCount + 1 
+            WHERE p.id = :postId
+            """)
     void incrementLikeCount(@Param("postId") Long postId);
 
-    @Modifying
-    @Query("UPDATE Post p SET p.likeCount = p.likeCount - 1 WHERE p.id = :postId")
+    @Modifying(clearAutomatically = true)
+    @Query("""
+            UPDATE Post p
+            SET p.likeCount = CASE WHEN p.likeCount > 0
+            THEN p.likeCount - 1
+            ELSE 0 END
+            WHERE p.id = :postId
+        """)
     void decrementLikeCount(@Param("postId") Long postId);
 
     @Query("SELECT p.likeCount FROM Post p WHERE p.id = :postId")
