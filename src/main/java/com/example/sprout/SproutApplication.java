ea975b73b7e884b0cb780cf620697bb27ab2adda
전채연 <ami02074657@gmail.com>
2026-07-25 15:42:46 +0900

    fix: JVM 기본 타임존을 Asia/Seoul로 고정

--- src/main/java/com/example/sprout/SproutApplication.java
+++ src/main/java/com/example/sprout/SproutApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @EnableCaching
 @SpringBootApplication
 public class SproutApplication {
 
 	public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(SproutApplication.class, args);
 	}
 
